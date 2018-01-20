b7533c24d9aaf654c0172b4c9f9448d25b42b040
Stefan Huber <sh@signalwerk.ch>
2018-01-20 12:32:32 +0100

    Update brew.sh

--- run/brew.sh
+++ run/brew.sh
@@ -32,6 +32,7 @@ brew install pandoc
 # command line tool for managing dock items 
 brew install dockutil
 brew install lftp
+brew install ffmpeg --with-vpx --with-vorbis --with-libvorbis --with-vpx --with-vorbis --with-theora --with-libogg --with-libvorbis --with-gpl --with-version3 --with-nonfree --with-postproc --with-libaacplus --with-libass --with-libcelt --with-libfaac --with-libfdk-aac --with-libfreetype --with-libmp3lame --with-libopencore-amrnb --with-libopencore-amrwb --with-libopenjpeg --with-openssl --with-libopus --with-libschroedinger --with-libspeex --with-libtheora --with-libvo-aacenc --with-libvorbis --with-libvpx --with-libx264 --with-libxvid
 
 
 # Apple Store -cli
