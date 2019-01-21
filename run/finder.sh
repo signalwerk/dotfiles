f6e252312d336ff540adc5082046198b303af87a
Stefan Huber <sh@signalwerk.ch>
2019-01-21 15:09:00 +0100

    ADD: some non working stuff 😕

--- run/finder.sh
+++ run/finder.sh
@@ -27,6 +27,18 @@ defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Desk
 # Four-letter codes for the other view modes: 'icnv', 'clmv', 'Flwv', 'Nlsv'
 defaults write com.apple.finder FXPreferredViewStyle -string clmv
 
+
+
+
+## NOT WORKING!!!!
+# Change "Move focus to next window" shortcut to ⌘<
+# /usr/libexec/PlistBuddy -c "Set :AppleSymbolicHotKeys:51:value:parameters:0 60" ~/Library/Preferences/com.apple.symbolichotkeys.plist
+# /usr/libexec/PlistBuddy -c "Print :AppleSymbolicHotKeys:51:value:parameters:0" ~/Library/Preferences/com.apple.symbolichotkeys.plist
+
+
+
+
+
 # ----------------------------------------------------------------------------------------
 # Battery Percentage in status bar
 defaults write com.apple.menuextra.battery ShowPercent -bool true
