094c9f6f662c2c53be9e2c400aec8f69ccb3b2ff
signalwerk <sh@signalwerk.ch>
2019-05-04 20:47:08 +0200

    MOD: open terminas

--- include/fs_helpers.sh
+++ include/fs_helpers.sh
@@ -6,7 +6,6 @@ deleteFs() {
   local _dir="${1}"
   /bin/rm -fr "${_dir}" &>/dev/null
 }
-
 linkToDestination() {
   /bin/ln -sfn "${1}" "${2}"  || echo -r "Error linking ${1} to ${2}."
 }
--- root.sh
+++ root.sh
@@ -6,24 +6,25 @@ mkdir -p  ~/.dotfiles/
 cd ~/.dotfiles/
 curl --progress-bar --location 'https://github.com/signalwerk/dotfiles/archive/master.tar.gz' | /usr/bin/tar -x --strip-components 1
 
+# Load Functions
+eval "$(/usr/bin/find "./include" -iname '*.sh' -exec echo . '{};' \;)"
 
 # run all parts of the setup
-. ./run/brew.sh
-. ./run/node.sh
+task ~/.dotfiles/run/brew.sh
+task ~/.dotfiles/run/node.sh
 
 # can start pretty early
-. ./run/appstore.sh
+task ~/.dotfiles/run/appstore.sh
 
 # can be run withouth the scripts above
-. ./run/ssh.sh
-. ./run/mac.sh
-. ./run/mail.sh
-. ./run/finder.sh
-. ./run/python.sh
-. ./run/git.sh
-. ./run/github.sh
-. ./run/atom.sh
-
+task ~/.dotfiles/run/ssh.sh
+task ~/.dotfiles/run/mac.sh
+task ~/.dotfiles/run/mail.sh
+task ~/.dotfiles/run/finder.sh
+task ~/.dotfiles/run/python.sh
+task ~/.dotfiles/run/git.sh
+task ~/.dotfiles/run/github.sh
+task ~/.dotfiles/run/atom.sh
 
 
 
