aa266585715b71b2bd037f0b00fd9f6002e32b26
signalwerk <sh@signalwerk.ch>
2019-05-04 21:05:08 +0200

    ADD: tasks on line basis

--- root.sh
+++ root.sh
@@ -7,34 +7,22 @@ cd ~/.dotfiles/
 curl --progress-bar --location 'https://github.com/signalwerk/dotfiles/archive/master.tar.gz' | /usr/bin/tar -x --strip-components 1
 
 # Load Functions
-eval "$(/usr/bin/find "./include" -iname '*.sh' -exec echo . '{};' \;)"
+# eval "$(/usr/bin/find "./include" -iname '*.sh' -exec echo . '{};' \;)"
 
 # run all parts of the setup
-task ~/.dotfiles/run/brew.sh
-task ~/.dotfiles/run/node.sh
+osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/brew.sh"'
+osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/node.sh"'
 
 # can start pretty early
-task ~/.dotfiles/run/appstore.sh
+osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/appstore.sh"'
 
 # can be run withouth the scripts above
-task ~/.dotfiles/run/ssh.sh
-task ~/.dotfiles/run/mac.sh
-task ~/.dotfiles/run/mail.sh
-task ~/.dotfiles/run/finder.sh
-task ~/.dotfiles/run/python.sh
-task ~/.dotfiles/run/git.sh
-task ~/.dotfiles/run/github.sh
-task ~/.dotfiles/run/atom.sh
-
-
-
-
-. ./run/dock.sh
-
-
-
-
-# --------------------------------------------------
-
-# link your installation file to setup.sh in current folder
-# linkToDestination ~/Dropbox/__PREFS__/data/setup.sh ./setup.sh
+osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/ssh.sh"'
+osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/mac.sh"'
+osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/mail.sh"'
+osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/finder.sh"'
+osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/python.sh"'
+osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/git.sh"'
+osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/github.sh"'
+osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/atom.sh"'
+osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/dock.sh"'
