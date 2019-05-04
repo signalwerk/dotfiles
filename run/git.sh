1c0a82354b5161590a049dde25d5de87958eb4b3
signalwerk <sh@signalwerk.ch>
2019-05-04 20:34:28 +0200

    ADD: default git name

--- run/git.sh
+++ run/git.sh
@@ -6,3 +6,7 @@ while [ ! -f ~/.dotfiles/.brew-installed ]; do sleep 1; done
 
 echo .DS_Store > ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
+
+git config --global user.name "signalwerk"
+
+git config --global user.email sh@signalwerk.ch
