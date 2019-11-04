4efa25eb036f5e43bd4cdfb91df7488ca1394a0e
Stefan Huber <sh@signalwerk.ch>
2019-11-04 23:57:11 +0100

    Update bash.sh

--- run/bash.sh
+++ run/bash.sh
@@ -5,3 +5,4 @@ chmod +x $HOME/Dropbox/Apps/Bash/bin/prettier-fusion-html.sh
 ln -s $HOME/Dropbox/Apps/Bash/bin/prettier-fusion-html.sh $HOME/Dropbox/Apps/Bash/bin/pfh
 echo "sed -e 's/:/---semicolon---/' | prettier --stdin-filepath foo.html | sed -e 's/---semicolon---/:/'" > $HOME/Dropbox/Apps/Bash/bin/prettier-fusion-html.sh
 echo 'PATH=$PATH:$HOME/Dropbox/Apps/Bash/bin/' >>~/.bash_profile
+echo 'alias lls="exa --long --header --git"' >>~/.bash_profile
