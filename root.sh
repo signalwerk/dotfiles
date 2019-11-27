e469102b491acb7c03ff0b01dfeee0705e1e9586
Stefan Huber <sh@signalwerk.ch>
2019-11-27 22:16:16 +0100

    Update root.sh

--- root.sh
+++ root.sh
@@ -11,18 +11,18 @@ curl --progress-bar --location 'https://github.com/signalwerk/dotfiles/archive/m
 
 # run all parts of the setup
 osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/brew.sh"'
-osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/node.sh"'
+# osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/node.sh"'
 
 # can start pretty early
 osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/appstore.sh"'
 
 # can be run withouth the scripts above
-osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/ssh.sh"'
-osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/mac.sh"'
-osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/mail.sh"'
-osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/finder.sh"'
-osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/python.sh"'
-osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/git.sh"'
-osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/github.sh"'
-osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/atom.sh"'
-osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/dock.sh"'
+# osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/ssh.sh"'
+# osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/mac.sh"'
+# osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/mail.sh"'
+# osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/finder.sh"'
+# osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/python.sh"'
+# osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/git.sh"'
+# osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/github.sh"'
+# osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/atom.sh"'
+# osascript -e 'tell app "Terminal" to do script "sh ~/.dotfiles/run/dock.sh"'
