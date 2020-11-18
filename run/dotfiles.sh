ca72fd9e719255dfd03a442c22363e2d54ce9120
Stefan Huber <sh@signalwerk.ch>
2020-11-18 14:13:52 +0100

    Update dotfiles.sh

--- run/dotfiles.sh
+++ run/dotfiles.sh
@@ -5,5 +5,6 @@ if [ ! -d "~/CODE/dotfiles" ]; then
   mkdir -p ~/CODE/
   cd ~/CODE/
 
-  git clone git@github.com:signalwerk/dotfiles.git
+  # git clone git@github.com:signalwerk/dotfiles.git
+  curl --progress-bar --location 'https://github.com/signalwerk/dotfiles/archive/master.tar.gz' | /usr/bin/tar -x --strip-components 1
 fi
