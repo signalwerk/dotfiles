625fcfa833d488da1233db6fe7571d38ab2aa4c6
signalwerk <sh@signalwerk.ch>
2021-06-03 00:39:44 +0200

    MOD: adjust node and zsh integration

--- run/node.sh
+++ run/node.sh
@@ -7,28 +7,24 @@ fi
 if [ ! -f ~/.dotfiles/.nvm--installed ]; then
   brew install nvm
 
+  echo '# -- nvm - start --' >> "$HOME/.zprofile"
+  echo 'export NVM_DIR="$HOME/.nvm"' >> "$HOME/.zprofile"
+  echo '[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm' >> "$HOME/.zprofile"
+  echo '[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion' >> "$HOME/.zprofile"
+  echo '# -- nvm - end --' >> "$HOME/.zprofile"
+
   # install yarn
   brew install yarn
-
-  # after in your ~/.zshrc or in .bash_profile if your use bash shell:
-  echo 'export NVM_DIR="$HOME/.nvm"' >>~/.zshrc
-  echo 'source "$(brew --prefix nvm)/nvm.sh"' >>~/.zshrc
-
-  # reload profile
-  source ~/.zshrc
-
-  # auto complete
-  # npm completion >> ~/.bashrc
-  npm completion >> ~/.zshrc
-
-  # reload bash profile
-  source ~/.zshrc
+  
+  # globals
+  npm install --global prettier
+  npm install --global eslint
 
   touch ~/.dotfiles/.nvm-installed
 fi
 
 # nvm install 0.12
-# nvm install 8
+nvm install 8
 nvm install 10
 nvm install 12
 nvm alias default 12
